331e2f8bb8e40d3d48496d86bf76dbc45fc1a5b5
Ishfaq <Ishfaq@192.168.1.3>
2023-12-07 13:27:49 +0600

    Lunch sauceDemo and logIn

---
+++ SeleniumTesting/src/test/java/SeleniumTesting/ProductPurchase_TestNG.java
@@ -0,0 +1,31 @@
+package SeleniumTesting;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class ProductPurchase_TestNG {
+	
+	WebDriver driver;
+	@BeforeTest
+	public void init()
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+	}
+	
+	@Test(priority=1)
+	public void logIn() // logIn using relative path
+	{
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("secret_sauce");
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+	}
+}
