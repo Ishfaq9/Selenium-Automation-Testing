d2b07818f5e63159f56067fc29b6c91491e82882
Ishfaq <Ishfaq@192.168.1.3>
2023-12-07 13:08:28 +0600

    Try Testing This and Sauce Demo test

---
+++ SeleniumTesting/src/test/java/SeleniumTesting/CheckUrl_SauceDemo.java
@@ -0,0 +1,43 @@
+package SeleniumTesting;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+
+
+public class CheckUrl_SauceDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		////Initialization
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver =new ChromeDriver();
+		
+		// Assignment-1
+		
+		driver.get("https://www.saucedemo.com/");
+		driver.manage().window().maximize();
+		
+		String expected = "https://www.saucedemo.com/";
+		
+		// Type-1 if-else
+		if(driver.getCurrentUrl().equals(expected))
+		{
+			System.out.println("Correct Url");
+		}
+		else 
+		{
+			System.out.println("Incorrect Url");
+		}
+		
+		//type-2 Assert
+		Assert.assertEquals(expected,driver.getCurrentUrl());
+		
+
+	}
+
+}
